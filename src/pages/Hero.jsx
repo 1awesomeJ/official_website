a64ba377282a1415348ffa9e0e9b119e10050cf8
cloobtech <belkid98@gmail.com>
2025-04-17 10:54:44 +0100

    Official Website

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import Nav from "../components/Nav";
-import hero_bg from "../assets/hero_bg.jpg";
 
 const Hero = () => {
   return (
-    <div
-      className="h-screen relative"
-      style={{ backgroundImage: `url(${hero_bg})`, backgroundSize: "cover" }}
-    >
+    <div className="h-screen relative overflow-hidden bg-gray-100">
       {/* Nav */}
       <Nav />
+
+      {/* Floating Gradient Background */}
+      <div className="absolute inset-0 z-0">
+        <div className="w-[200%] h-[200%] bg-gradient-to-r from-blue-800 via-pink-700 to-purple-700 animate-gradientBlur opacity-50"></div>
+      </div>
+
       {/* Overlay */}
       <div
-        className="absolute inset-0"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+        className="absolute inset-0 z-1"
+        style={{ backgroundColor: "rgba(255, 255, 255, 0.6)" }}
       ></div>
 
       {/* Content */}
-      <div className=" text-center relative z-1 text-white h-full flex items-center justify-center">
-        <div className="border max-w-[600px] w-[80%]  bg-white/20 rounded-lg shadow-lg"> 
-          <h1 className="sm:text-5xl font-bold border-none border-gray-100 px-6 py-2 text-blue-300">
+      <div className="text-center relative z-10 text-gray-800 h-full flex items-center justify-center">
+        <div className="border max-w-[600px] w-[80%] bg-white/70 rounded-lg shadow-lg">
+          <h1 className="text-3xl sm:text-5xl font-bold border-none border-gray-100 px-6 py-2 text-blue-500">
             CENTRUMSCIEN
           </h1>
           <p className="text-sm p-3">
